0662f0291b7b968ddf518f396f6f3f9e0bbaf956
piotr.minkowski <piotr.minkowski@gmail.com>
2017-10-05 11:19:29 +0200

    add custom counter metrics

--- src/main/java/pl/piomin/services/boot/controller/PersonController.java
+++ src/main/java/pl/piomin/services/boot/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.piomin.services.boot.model.Person;
+import pl.piomin.services.boot.service.PersonCounterService;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
 	private List<Person> persons = new ArrayList<>();
+	
+	@Autowired
+	PersonCounterService counterService;
 	
 	@GetMapping
 	public List<Person> findAll() {
@@ -35,6 +40,7 @@ public class PersonController {
 	public Person add(@RequestBody Person p) {
 		p.setId((long) (persons.size()+1));
 		persons.add(p);
+		counterService.countNewPersons();
 		return p;
 	}
 	
@@ -42,6 +48,7 @@ public class PersonController {
 	public void delete(@RequestParam("id") Long id) {
 		List<Person> p = persons.stream().filter(it -> it.getId().equals(id)).collect(Collectors.toList());
 		persons.removeAll(p);
+		counterService.countDeletedPersons();
 	}
 	
 	@PutMapping
