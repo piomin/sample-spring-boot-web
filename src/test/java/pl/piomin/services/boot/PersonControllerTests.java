3763e269f6251f2af46d7588f28ac516ef435d85
piomin <piotr.minkowski@gmail.com>
2025-12-29 01:28:10 +0100

    Add @AutoConfigureRestTestClient

--- src/test/java/pl/piomin/services/boot/PersonControllerTests.java
+++ src/test/java/pl/piomin/services/boot/PersonControllerTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.client.RestTestClient;
 import org.springframework.web.context.WebApplicationContext;
@@ -13,24 +14,25 @@ import pl.piomin.services.boot.model.Person;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureRestTestClient
 public class PersonControllerTests {
 
     private static final String API_PATH = "/persons";
 
-    private RestTestClient restTemplate;
-
-    public PersonControllerTests(WebApplicationContext context) {
-        this.restTemplate = RestTestClient.bindToApplicationContext(context)
-                .baseUrl(API_PATH)
-                .build();
-    }
+//    private RestTestClient restTemplate;
+//
+//    public PersonControllerTests(WebApplicationContext context) {
+//        this.restTemplate = RestTestClient.bindToApplicationContext(context)
+//                .baseUrl(API_PATH)
+//                .build();
+//    }
 
     @Test
     @Order(1)
-    void add() {
-        restTemplate.post().body(Instancio.create(Person.class))
+    void add(@Autowired RestTestClient restTestClient) {
+        restTestClient.post().uri(API_PATH).body(Instancio.create(Person.class))
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody(Person.class)
@@ -39,8 +41,8 @@ public class PersonControllerTests {
 
     @Test
     @Order(2)
-    void findAll() {
-        restTemplate.get()
+    void findAll(@Autowired RestTestClient restTestClient) {
+        restTestClient.get().uri(API_PATH)
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody(Person[].class)
@@ -49,8 +51,8 @@ public class PersonControllerTests {
 
     @Test
     @Order(2)
-    void findById() {
-        restTemplate.get().uri("/{id}", 1L)
+    void findById(@Autowired RestTestClient restTestClient) {
+        restTestClient.get().uri(API_PATH + "/{id}", 1L)
                 .exchange()
                 .expectBody(Person.class)
                 .value(person -> assertNotNull(person.getId()));
@@ -58,17 +60,17 @@ public class PersonControllerTests {
 
     @Test
     @Order(3)
-    void update() {
-        Person person = restTemplate.get().uri("/{id}", 1L)
+    void update(@Autowired RestTestClient restTestClient) {
+        Person person = restTestClient.get().uri(API_PATH + "/{id}", 1L)
                 .exchange()
                 .returnResult(Person.class)
                 .getResponseBody();
         person.setFirstName("Updated");
-        restTemplate.put().uri("/{id}", 1L)
+        restTestClient.put().uri(API_PATH + "/{id}", 1L)
                 .body(person)
                 .exchange()
                 .expectStatus().is2xxSuccessful();
-        restTemplate.get().uri("/{id}", 1L)
+        restTestClient.get().uri(API_PATH + "/{id}", 1L)
                 .exchange()
                 .expectBody(Person.class)
                 .value(personUpdated -> assertEquals("Updated", personUpdated.getFirstName()));
@@ -76,11 +78,11 @@ public class PersonControllerTests {
 
     @Test
     @Order(4)
-    void delete() {
-        restTemplate.delete().uri("/{id}", 1L)
+    void delete(@Autowired RestTestClient restTestClient) {
+        restTestClient.delete().uri(API_PATH + "/{id}", 1L)
                 .exchange()
                 .expectStatus().is2xxSuccessful();
-        restTemplate.get().uri("/{id}", 1L)
+        restTestClient.get().uri(API_PATH + "/{id}", 1L)
                 .exchange()
                 .expectStatus().is5xxServerError();
     }
